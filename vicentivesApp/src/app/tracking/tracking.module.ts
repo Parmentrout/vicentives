29351b18951779b0e1deea7fe1fefc392ba60ccb
Patrick Armentrout <patrick.armentrout@gmail.com>
2021-11-07 19:44:06 -0700

    Fix build

--- vicentivesApp/src/app/tracking/tracking.module.ts
+++ vicentivesApp/src/app/tracking/tracking.module.ts
@@ -3,12 +3,16 @@ import { CommonModule } from '@angular/common';
 
 import { TrackingRoutingModule } from './tracking-routing.module';
 import { TrackingComponent } from './tracking.component';
+import { FormsModule } from '@angular/forms';
+import { IonicModule } from '@ionic/angular';
 
 
 @NgModule({
   declarations: [TrackingComponent],
   imports: [
     CommonModule,
+    FormsModule,
+    IonicModule,
     TrackingRoutingModule
   ]
 })
