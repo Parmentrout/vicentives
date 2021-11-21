77e1ef67b7657375f5cceb791d433477cb2fb744
Patrick Armentrout <patrick.armentrout@gmail.com>
2021-11-21 14:52:49 -0700

    MVP Is complete!

--- vicentivesApp/src/app/calibration/calibration.component.ts
+++ vicentivesApp/src/app/calibration/calibration.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { UserCalibration } from '../models/user-calibration.model';
+import { TrackingService } from '../services/tracking.service';
 import { UserService } from '../services/user.service';
 
 @Component({
@@ -11,16 +12,36 @@ import { UserService } from '../services/user.service';
 export class CalibrationComponent implements OnInit {
 
   user: UserCalibration;
+  formReady: boolean;
 
-  constructor(private router: Router, private userService: UserService) { 
-    this.user = this.userService.getUser();
+  constructor(
+    private router: Router, 
+    private userService: UserService, 
+    private trackingService: TrackingService) { 
+      this.user = this.userService.getUser();
   }
 
   ngOnInit() {}
 
   navigate(){
     this.userService.saveToSession(this.user);
+    this.trackingService.deleteSessionData();
+    this.trackingService.createNewTrackingData(this.user);
     this.router.navigate(['/home'])
+  }
+
+  clearSessionData() {
+    this.trackingService.deleteSessionData();
+    this.user = new UserCalibration();
+    this.userService.deleteSessionData();
+  }
+
+  isFormReady() {
+    return this.user && this.user.name 
+      && this.user.vice 
+      && this.user.viceAmount > 0
+      && this.user.virtueAmount > 0
+      && this.user.virtues
   }
 
 }
